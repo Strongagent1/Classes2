8ac3f834f89f5dcaf4dc1f0de38820bad1877e53
Kyle Jenne <strongagent1@gmail.com>
2023-01-05 09:59:08 -0500

    Reworked the Classes exercise -KJ

---
+++ Classes-Exercise/Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.Make = "GMC";
+            car1.Model = "YukonXL";
+            car1.Year = 2021;
+
+            Console.WriteLine($" The vehicle is a {car1.Year} {car1.Make} {car1.Model}.");
+        }
+    }
+}
